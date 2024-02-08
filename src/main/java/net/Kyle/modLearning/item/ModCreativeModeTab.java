89e3b627477c0367b763289314bbc49c9fc7ac72
schow070 <schow070@d.umn.edu>
2024-02-07 19:33:21 -0600

    Added titanium raw ore, ingot and pickaxe

--- src/main/java/net/Kyle/modLearning/item/ModCreativeModeTab.java
+++ src/main/java/net/Kyle/modLearning/item/ModCreativeModeTab.java
@@ -2,13 +2,13 @@ package net.Kyle.modLearning.item;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import org.checkerframework.checker.units.qual.C;
 
 public class ModCreativeModeTab {
     public static final CreativeModeTab Example_mod_tab = new CreativeModeTab("ExampleCreativeTab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.test.get());
+            return new ItemStack(ModItems.titanium_ingot.get());
         }
+
     };
 }
--- src/main/java/net/Kyle/modLearning/item/ModItems.java
+++ src/main/java/net/Kyle/modLearning/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.Kyle.modLearning.item;
 
 import net.Kyle.modLearning.TutorialMod;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,7 +11,13 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
-    public static final RegistryObject<Item> test = ITEMS.register("placeholder",
+    public static final RegistryObject<Item> titanium_ingot = ITEMS.register("titanium_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.Example_mod_tab)));
+
+ public static final RegistryObject<Item> titanium_pickaxe = ITEMS.register("titanium_pickaxe",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.Example_mod_tab)));
+
+ public static final RegistryObject<Item> raw_titanium = ITEMS.register("raw_titanium",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.Example_mod_tab)));
 
 
