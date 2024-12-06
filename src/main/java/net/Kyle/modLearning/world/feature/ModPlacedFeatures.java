94caf4b26988c5afcf757522de45a690ab58e724
schow070 <schow070@d.umn.edu>
2024-12-06 14:31:05 -0600

    Update version
    updated from 1.19 to 1.19.2. Tweaked ore gen rate to give greater amount of veins per chunk.

--- src/main/java/net/Kyle/modLearning/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/Kyle/modLearning/world/feature/ModPlacedFeatures.java
@@ -19,7 +19,7 @@ public class ModPlacedFeatures {
 
     public static final RegistryObject<PlacedFeature> TITANIUM_ORE = PLACED_FEATURES.register("titanium_ore_placed",
             () -> new PlacedFeature(ModConfiguredFeatures.TITANIUM_ORE.getHolder().get(),
-                    commonOrePlacement(7, //VeinsPerChunk
+                    commonOrePlacement(12, //VeinsPerChunk
                             HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-80), VerticalAnchor.aboveBottom(80)))));
 
     public static List<PlacementModifier> orePlacement(PlacementModifier p_195347_, PlacementModifier p_195348_) {
